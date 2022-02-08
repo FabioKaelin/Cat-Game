0dfa70df0e5a47d2ef46cd9acc39cb7975cf5255
FabioKaelin <90179796+FabioKaelin@users.noreply.github.com>
2022-02-08 09:49:13 +0100

    Verschoben

--- catController.js
+++ catController.js
@@ -61,32 +61,3 @@ const move = function(){
 }
 window.addEventListener("keydown", move)
 window.addEventListener("keyup", move)
-
-let speed = 1
-let adler = document.getElementById("adler")
-
-var i = 0;
-// var j = 0;
-
-function myLoop() {
-    setTimeout(function() {
-        adler.style.bottom = (i + "px")
-        console.log(i + "adler")
-        i++;
-        if (i < 100) {
-            myLoop();
-        }
-    }, 10)
-}
-
-
-
-
-myLoop();
-console.log("myloop end")
-// CatJump();
-
-
-
-//Hallo Jon
-//Hallo FAbio
