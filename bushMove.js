cacf6c12ce3b75c61960d8250310c393b1048d11
Jon Landa <jonlanda06@gmail.com>
2022-02-08 11:47:46 +0100

    merged

--- bushMove.js
+++ bushMove.js
@@ -7,22 +7,12 @@ k = 600;
 function moveBush() {
     bush.style.display = "block"
     setTimeout(function() {
-<<<<<<< HEAD
-        bush.style.left = (j + "px")
-
-        j--;
-        j--;
-        j--;
-        j--;
-        if (j > 0) {
-=======
         bush.style.left = (k + "px")
         if (k > -50) {
             k--;
             k--;
             k--;
             k--;
->>>>>>> 9a44a33561d2ecc20cd95e8dcfef7ffce479b7f7
             moveBush();
         } else {
             bush.style.display = "none";
@@ -30,8 +20,4 @@ function moveBush() {
     }, 2)
 }
 
-<<<<<<< HEAD
 moveBush()
-=======
-moveBush();
->>>>>>> 9a44a33561d2ecc20cd95e8dcfef7ffce479b7f7
