f74684c5981cce4d997816b4c429f4c376754ce0
FabioKaelin <90179796+FabioKaelin@users.noreply.github.com>
2022-02-08 14:39:35 +0100

    Modify

--- elseController.js
+++ elseController.js
@@ -1,26 +1,26 @@
 
-let speed = 1
-let adler = document.getElementById("adler")
+// let speed = 1
+// let adler = document.getElementById("adler")
 
-var i = 0;
+// var i = 0;
 // var j = 0;
 
-function myLoop() {
-    setTimeout(function() {
-        adler.style.bottom = (i + "px")
-        // console.log(i + "adler")
-        i++;
-        if (i < 100) {
-            myLoop();
-        }
-    }, 10)
-}
+// function myLoop() {
+//     setTimeout(function() {
+//         adler.style.bottom = (i + "px")
+//         // console.log(i + "adler")
+//         i++;
+//         if (i < 100) {
+//             myLoop();
+//         }
+//     }, 10)
+// }
 
 
 
 
-myLoop();
-console.log("myloop end")
+// myLoop();
+// console.log("myloop end")
 // CatJump();
 
 
