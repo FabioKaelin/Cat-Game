3173b18bb5e44baa263e9a3b60cca92b646d7e65
FabioKaelin <90179796+FabioKaelin@users.noreply.github.com>
2022-02-08 11:48:27 +0100

    Modified

--- bushMove.js
+++ bushMove.js
@@ -6,29 +6,15 @@ k = 600;
 
 function moveBush() {
     setTimeout(function() {
-<<<<<<< HEAD
-        bush.style.left = (j + "px")
-
-        j--;
-        j--;
-        j--;
-        j--;
-        if (j > 0) {
-=======
         bush.style.left = (k + "px")
         if (k > 0) {
             k--;
             k--;
             k--;
             k--;
->>>>>>> 9a44a33561d2ecc20cd95e8dcfef7ffce479b7f7
             moveBush();
         }
     }, 2)
 }
 
-<<<<<<< HEAD
-moveBush()
-=======
 moveBush();
->>>>>>> 9a44a33561d2ecc20cd95e8dcfef7ffce479b7f7
