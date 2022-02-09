5a254685b435fdea5a33a5b09486c88ab49e038f
Jon Landa <jonlanda06@gmail.com>
2022-02-09 10:43:47 +0100

    added night and day animation and colors

--- TagesZyklus.js
+++ TagesZyklus.js
@@ -1,5 +1,7 @@
 let mond = document.getElementById("Mond")
 let sun = document.getElementById("Sun")
+let body = document.body
+let div = document.getElementById("GameContent")
 sun.style.display = "none"
 mond.style.display = "none"
 
@@ -8,20 +10,32 @@ console.log(mond)
 console.log(sun)
 function zyklus() {
     setTimeout(function() {
-        if (t < 50) {
+        if (t < 5000) {
             t++;
-            if (t < 25) {
+            if (t < 2500) {
                 mond.style.display = "block"
+                mond.style.left = `${0.035 * t}%`
                 sun.style.display = "none"
+                body.style.backgroundColor = "dimgrey"
+                div.style.backgroundColor = "rgb(43, 43, 43)"
             } else {
                 mond.style.display = "none"
                 sun.style.display = "block"
+                sun.style.left = `${0.03 * (t - 2500)}%`
+                body.style.backgroundColor = "rgb(55, 101, 255)"
+                div.style.backgroundColor = "rgb(0, 225, 255)"
             }
         } else {
             t = 0;
         }
         zyklus()
-    }, 1000)
+    }, 10)
 }
 
 zyklus()
+
+let star = document.getElementsByClassName("star")
+
+function star() {
+
+}
