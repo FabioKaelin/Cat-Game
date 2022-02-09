b7bc81c03a78dfdab7ee593b13286f04920be41a
Jon Landa <jonlanda06@gmail.com>
2022-02-09 15:30:21 +0100

    stars move

--- TagesZyklus.js
+++ TagesZyklus.js
@@ -6,13 +6,25 @@ sun.style.display = "none"
 mond.style.display = "none"
 let night = false;
 
+
+xcords0 = Math.floor(Math.random() * 2000);
+xcords1 = Math.floor(Math.random() * 2000);
+xcords2 = Math.floor(Math.random() * 2000);
+console.log(` ${xcords0} ${xcords1} ${xcords2} `)
+
 let t = 0;
 function zyklus() {
     setTimeout(function() {
         if (t < 5000) {
             t++;
+            xcords0++
+            xcords1++
+            xcords2++
             if (t < 2500) {
                 night = true;
+                stars[0].style.left = `${0.035 * xcords0}%`
+                stars[1].style.left = `${0.035 * xcords1}%`
+                stars[2].style.left = `${0.035 * xcords2}%`
                 stars[0].style.display = "block"
                 stars[1].style.display = "block"
                 stars[2].style.display = "block"
@@ -42,22 +54,25 @@ function zyklus() {
 zyklus()
 
 let stars = document.getElementsByClassName("star")
+console.info(stars)
 
 function star() {
     if (night = true) {
 
-        ycords1 = Math.floor(Math.random() * 75);
+        console.log("skibidabndodo")
+        ycords1 = Math.floor(Math.random() * 50);
         stars[0].style.top = `${ycords1}%`
 
-        ycords2 = Math.floor(Math.random() * 75);
+        ycords2 = Math.floor(Math.random() * 50);
         stars[1].style.top = `${ycords2}%`
 
-        ycords3 = Math.floor(Math.random() * 75);
+        ycords3 = Math.floor(Math.random() * 50);
         stars[2].style.top = `${ycords3}%`
+        console.log(` ${ycords1} ${ycords2} ${ycords3} `)
 
-        stars[0].style.left = `${0.03 * (t - 2500)}%`
-        stars[1].style.left = `${0.03 * (t - 2500)}%`
-        stars[2].style.left = `${0.03 * (t - 2500)}%`
+        stars[0].style.left = `${0.035 * t}%`
+        stars[1].style.left = `${0.035 * t}%`
+        stars[2].style.left = `${0.035 * t}%`
     } else {
     }
 }
