da3dcaae30782a7e9ac2c549f421ac0b79be38a8
FabioKaelin <90179796+FabioKaelin@users.noreply.github.com>
2022-02-10 08:32:50 +0100

    Bird Hitbox fixed

--- adlerMove.js
+++ adlerMove.js
@@ -6,7 +6,7 @@ let adlerXArray = []
 function moveadler(adlerX, Currentadler, adlers, adlerY) {
     adlers[Currentadler].style.left = adlerX
     adlers[Currentadler].style.display = "block"
-    if((adlerX < 20 && adlerX > 2) && (catY < adlerY && catY > (adlerY - ((sneakTrue + 1)*20)))){
+    if((adlerX < 10 && adlerX > 2) && (catY < adlerY-((sneakTrue + 1)*20) || catY > adlerY)){
         gameOver = true
     }
     if(gameOver){
@@ -16,11 +16,11 @@ function moveadler(adlerX, Currentadler, adlers, adlerY) {
         adlers[Currentadler].style.left = (adlerX + "%")
         if (adlerX > -5) {
             adlerX -= 0.5;
-            moveadler(adlerX, Currentadler, adlers);
+            moveadler(adlerX, Currentadler, adlers, adlerY);
         } else {
             adlers[Currentadler].style.display = "none";
         }
-    }, 2)
+    }, 10)
 }
 function spawnadler(adlerXArray, adlerNumber, adlers){
     if(gameOver){
@@ -30,8 +30,10 @@ function spawnadler(adlerXArray, adlerNumber, adlers){
     setTimeout(function(){
         if (Math.floor(Math.random() * 10) < 9){
             adlerXArray[adlerNumber] = 95;
-            adlers[adlerNumber].style.top = `${Math.floor(Math.random() * 75)}%`
-            moveadler(adlerXArray[adlerNumber], adlerNumber, adlers)
+            let adlerY = Math.floor(Math.random() * 75)
+            adlers[adlerNumber].style.top = `${adlerY}%`
+            console.log(adlerY)
+            moveadler(adlerXArray[adlerNumber], adlerNumber, adlers, adlerY)
             adlerNumber++
             if(adlerNumber == 3){
                 adlerNumber = 0
@@ -50,7 +52,7 @@ function DelayStart() {
 
     setTimeout(function(){
         spawnadler(adlerXArray, adlerNumber, adlers)
-    },50000)
+    },1000)
 }
 
 DelayStart()
